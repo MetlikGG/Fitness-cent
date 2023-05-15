6eda21d24cc363d8e8df4d252740029efa36c9ec
MetlikGG <kurochkinalexy@mail.ru>
2023-05-15 22:21:01 +0300

    Добавляет блок subscriptions

--- source/js/main.js
+++ source/js/main.js
@@ -1,10 +1,13 @@
 import {iosVhFix} from './utils/ios-vh-fix';
-import {initModals} from './modules/modals/init-modals';
+import {initTabs} from './modules/tabs/init-tabs';
 import {Form} from './modules/form-validate/form';
 
 // ---------------------------------
 
 window.addEventListener('DOMContentLoaded', () => {
+  const tabs = document.querySelector('[data-tabs="content"]');
+
+  tabs.classList.remove('tabs__content--nojs');
 
   // Utils
   // ---------------------------------
@@ -17,7 +20,7 @@ window.addEventListener('DOMContentLoaded', () => {
   // все скрипты должны быть в обработчике 'DOMContentLoaded', но не все в 'load'
   // в load следует добавить скрипты, не участвующие в работе первого экрана
   window.addEventListener('load', () => {
-    initModals();
+    initTabs();
     const form = new Form();
     window.form = form;
     form.init();
